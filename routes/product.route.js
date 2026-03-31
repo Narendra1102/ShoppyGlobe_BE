9a3ce4adcd02400a860192b2a4d0a80166cad0fe
Narendra1102 <narendra.v02111@gmail.com>
2026-03-31 16:23:21 +0530

    Storing and Fetching the products

---
+++ routes/product.route.js
@@ -0,0 +1,13 @@
+import { getProductById, getProducts, postProductData } from "../controller/product.controller.js"
+
+
+export function productRoute(app){
+    //Fetch all products
+    app.get("/api/products",getProducts)
+
+    //Fetch product by id
+    app.get("/api/products/:id",getProductById)
+
+    //store product data
+    app.post("/api/product",postProductData)
+}
